18eee92591c2abf0dc7d9b2ca41aa95abac19fdb
Andrey-A-A <stuff-box@bk.ru>
2023-07-01 17:57:27 +0300

    страница моих митапов

--- liga-meetups/src/app/modules/meetups/meetups.module.ts
+++ liga-meetups/src/app/modules/meetups/meetups.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MeetupsComponent } from './meetups.component';
 import { MeetupComponent } from '../meetup/meetup.component';
+import { MyMeetupsComponent } from '../my-meetups/my-meetups.component';
 // import {MatButtonModule} from '@angular/material/button';
 import {MatPaginatorModule} from '@angular/material/paginator';
 
@@ -14,12 +15,14 @@ const materialModules = [
   declarations: [
     MeetupsComponent,
     MeetupComponent,
+    MyMeetupsComponent
   ],
   imports: [
     CommonModule,
       ...materialModules
   ],
   exports: [
+    MeetupComponent,
     MeetupsComponent,
       ...materialModules
   ]
