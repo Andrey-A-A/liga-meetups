fa850104d0d8ab775bc07d53d4d1005da790445b
Andrey Anisimov <76514999+Andrey-A-A@users.noreply.github.com>
2023-07-03 17:07:52 +0300

    функциональность поиска (#9)

--- liga-meetups/src/app/modules/meetups/meetups.module.ts
+++ liga-meetups/src/app/modules/meetups/meetups.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 import { MeetupsComponent } from './meetups.component';
 import { MeetupComponent } from '../meetup/meetup.component';
 import { MyMeetupsComponent } from '../my-meetups/my-meetups.component';
-// import {MatButtonModule} from '@angular/material/button';
+import { SearchComponent } from '../search/search.component';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 const materialModules = [
@@ -15,9 +16,11 @@ const materialModules = [
   declarations: [
     MeetupsComponent,
     MeetupComponent,
-    MyMeetupsComponent
+    MyMeetupsComponent,
+    SearchComponent,
   ],
   imports: [
+    ReactiveFormsModule,
     CommonModule,
       ...materialModules
   ],
